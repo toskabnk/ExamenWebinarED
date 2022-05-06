eda02ab6ffe13e8a68175983ea37db060f5b2d90
Antonio de la Rosa <santiagodelarosaantonio@gmail.com>
2022-05-06 18:25:29 +0200

    Fix: Baraja creada

--- src/main/java/com/svalero/entornos/Baraja.java
+++ src/main/java/com/svalero/entornos/Baraja.java
@@ -1,2 +1,24 @@
-package com.svalero.entornos;public class Baraja {
+package com.svalero.entornos;
+
+import java.util.ArrayList;
+
+public class Baraja {
+    private ArrayList<Carta> listaCartas;
+    public final int NUM_CARTAS = 52;
+
+    public Baraja(){
+        listaCartas = new ArrayList<>();
+    }
+
+    public void barajar(){
+
+    }
+
+    public Carta cogerCarta(){
+        return listaCartas.get(5);
+    }
+
+    public ArrayList<Carta> repartirMano(){
+        return listaCartas;
+    }
 }
